54d0f3d6ced808900d06f4f121068e7991dc5ce0
Cody Breene <Cody@cody-breenes-macbook-pro.local>
2013-11-23 18:10:43 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codyapp::Application.config.secret_token = '064da5a6a73c599fbaf400230c7fb36acd232c4fdf9f7e2456600e9cbac37dc1bd317c75231d7e8ec8c255cf333245e5b96e2217c9470b2e7e20c88468556329'
